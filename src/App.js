292c5f43a403de4aa880b0b45f33463f26a38b4f
atrus <atrus@localhost.localdomain>
2019-02-26 19:59:08 -0700

    Completed code given in App.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  // findMultiples() finds all of the numbers in a range given that are divisible by a specified 'divisor'
+  findMultiples(divisor, bottom, top) {
+    let multiples = [];
+    let start = this.findStart(divisor,bottom,top);
+
+    while(start < top) {
+      multiples.push(start);
+      start = start + divisor;
+    }
+
+    return multiples;
+  }
+
+  // findStart() finds the lowest number in the range that is divisible by 'divisor'
+  findStart(divisor, bottom, top) {
+    while(bottom < top) {
+      if(bottom % divisor === 0) {
+        return bottom;
+      }
+      bottom++;
+    }
+    return 0;
+  }
+
   render() {
+    let converter = require('number-to-words'); // number to words is a library used to convert a number into english words
+    let numberList = this.findMultiples(3,5,100); // finds <multiples of 3> between <5 and 100>
+    // the list of applicable numbers is reversed and converted to strings during the render operation
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {numberList.reverse().map((num) => <h4>{converter.toWords(num)}</h4>)}
       </div>
     );
   }
