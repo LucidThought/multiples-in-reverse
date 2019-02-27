e5d93fecd49211b1ced2955cd6e5319f389e224c
atrus <atrus@localhost.localdomain>
2019-02-26 20:07:26 -0700

    added to comment

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
   render() {
     let converter = require('number-to-words'); // number-to-words is a library used to convert a number into english words
     let numberList = this.findMultiples(3,5,100); // finds <multiples of 3> between <5 and 100>
-    // the list of applicable numbers is reversed and converted to strings during the render operation
+    // the list of applicable numbers is reversed (to show high to low) and converted to strings during the render operation
     return (
       <div className="App">
         {numberList.reverse().map((num) => <h4>{converter.toWords(num)}</h4>)}
